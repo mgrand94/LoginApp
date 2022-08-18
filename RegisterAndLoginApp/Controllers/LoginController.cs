2998807cbff8486ca8e2f5ce88d6d50af14bbd19
Your Name <Marcosemail2015@gmail.com>
2022-08-18 10:12:15 -0600

    Add project files.

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "mgrand94" && userModel.Password == "Cowboys94")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
