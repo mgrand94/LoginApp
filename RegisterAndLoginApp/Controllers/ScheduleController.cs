132c4c43c132363a4b6d308d81d0641b67e0a118
Your Name <Marcosemail2015@gmail.com>
2022-08-23 14:29:41 -0600

    ADDED: DETAILS VIEW ADDED: Scheduler ADDED: INDEX LIST

---
+++ RegisterAndLoginApp/Controllers/ScheduleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // list of appoinments
+        static List<ScheduleModel> appoinments = new List<ScheduleModel>();
+        public IActionResult Index()
+        {
+            return View(appoinments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(ScheduleModel appointment)
+        {
+            //adds to the appointments list
+            appoinments.Add(appointment);
+            return View("Details", appointment); 
+        }
+    }
+}
---
+++ RegisterAndLoginApp/Models/ScheduleModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegisterAndLoginApp.Models
+{
+    public class ScheduleModel
+    {
+        [Required] // All fields required
+        [DisplayName("Client's Full Name:")]
+        public string ClientName { get; set; }
+        [DisplayName("Client's Insurance:")]
+        public string ClientInsurance { get; set; }
+        [DisplayName("Client's Total hours:")]
+        public int TotalHours { get; set; }
+        [DisplayName("Client's Hours Left:")]
+
+        public int HoursUsed { get; set; }
+
+        [DisplayName("Appointment Date:")]
+
+        [DataType(DataType.Date)] //Appointment date
+        public DateTime AppointmentDate { get; set; }
+        [DisplayName("Therapist Assigned:")]
+        public string TherapistName { get; set; }
+    }
+}
